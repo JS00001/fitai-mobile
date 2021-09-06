be81c90a1d48ff8c111e279c82de051e67936521
JS <49812749+JS00001@users.noreply.github.com>
2021-09-06 15:08:28 -0400

    Workout Pages

--- src/screens/Subscreens/Workouts.Personalized.jsx
+++ src/screens/Subscreens/Workouts.Personalized.jsx
@@ -5,6 +5,28 @@ import Container from '../../components/Container';
 
 export default function WorkoutsPersonalized() {
     return (
-        <></>
+        <ScrollView>
+            <View style={styles.container}>
+                <Container>
+                    <Container.Title>Chest/Abs</Container.Title>
+                    <Container.Subtitle>Average Length - 1 hour 30 minutes</Container.Subtitle>
+                    <Container.Button>Preview</Container.Button>
+                    <Container.Button>Save Workout</Container.Button>
+                </Container>
+                <Container>
+                    <Container.Title>Chest/Abs</Container.Title>
+                    <Container.Subtitle>Average Length - 1 hour 30 minutes</Container.Subtitle>
+                    <Container.Button>Preview</Container.Button>
+                    <Container.Button>Save Workout</Container.Button>
+                </Container>
+            </View>
+        </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, 
+        alignItems: 'center',
+    }
+})
--- src/screens/Workouts.jsx
+++ src/screens/Workouts.jsx
@@ -1,20 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import WorkoutsPersonalized from './Subscreens/Workouts.Personalized';
-import WorkoutsYours from './Subscreens/Workouts.Yours';
-import SegmentControl from '../components/SegmentControl';
+import WorkoutsPersonalized from "./Subscreens/Workouts.Personalized";
+import WorkoutsYours from "./Subscreens/Workouts.Yours";
+import SegmentControl from "../components/SegmentControl";
 
 export default function Workouts() {
-
     const [pageIndex, setPageIndex] = React.useState(0);
 
     return (
         <>
-            <SegmentControl 
+            <SegmentControl
                 values={["Your Workouts", "Personalized"]}
                 onChange={(index) => setPageIndex(index)}
             />
             {pageIndex ? <WorkoutsPersonalized /> : <WorkoutsYours />}
         </>
-    )
+    );
 }
